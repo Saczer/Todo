5caf58b213727ef1da6fca6368c3476a744e1ad8
Michał Olszak <michaso65@gmail.com>
2018-01-25 21:57:12 +0100

    merge

--- app/src/main/java/pl/olszak/michal/todo/hello/MainActivity.kt
+++ app/src/main/java/pl/olszak/michal/todo/hello/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         mainViewModel = ViewModelProviders.of(this, viewModelFactory)
                 .get(MainViewModel::class.java)
 
-        binding.viewModel = mainViewModel.provideBindable()
+
     }
+
 }
